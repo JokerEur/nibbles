83a3cc7f22e7fb405ca45dc8c334424b6a6836a8
Ivan <2001joker2001@gmail.com>
2021-12-25 22:47:56 +0300

    remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ivan/Documents/workspace/Nibbles
-# Build directory: /home/ivan/Documents/workspace/Nibbles/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
